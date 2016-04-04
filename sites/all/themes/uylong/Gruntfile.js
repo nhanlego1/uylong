8bbc49e6260e36d68f41457b02e3680ee453ebcc
nhan <nhanlego1@gmail.com>
2016-04-04 22:56:17 +0700

    update

--- sites/all/themes/uylong/Gruntfile.js
+++ sites/all/themes/uylong/Gruntfile.js
@@ -1,58 +1,62 @@
-'use strict';
+module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
 
-module.exports = function (grunt) {
+  var theme_name = 'STARTER';
+  var base_theme_path = '../zurb_foundation';
+
+  var global_vars = {
+    theme_name: theme_name,
+    theme_css: 'css',
+    theme_scss: 'scss',
+    base_theme_path: base_theme_path
+  };
+
+  var bourbon = require('node-bourbon').includePaths;
+
+  // array of javascript libraries to include.
+  var jsLibs = [
+    '<%= global_vars.base_theme_path %>/js/vendor/placeholder.js',
+    '<%= global_vars.base_theme_path %>/js/vendor/fastclick.js'
+  ];
+
+  // array of foundation javascript components to include.
+  var jsFoundation = [
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.abide.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.accordion.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.alert.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.clearing.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.dropdown.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.equalizer.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.interchange.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.joyride.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.magellan.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.offcanvas.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.orbit.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.reveal.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.slider.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.tab.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.tooltip.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.topbar.js'
+  ];
+
+  // array of custom javascript files to include.
+  var jsApp = [
+    'js/_*.js'
+  ];
 
   grunt.initConfig({
-    watch: {
-      options: {
-        livereload: true
-      },
-      sass: {
-        files: ['sass/{,**/}*.{scss,sass}'],
-        tasks: ['compass:dev'],
-        options: {
-          livereload: false
-        }
-      },
-      registry: {
-        files: ['*.info', '{,**}/*.{php,inc}'],
-        tasks: ['shell'],
-        options: {
-          livereload: false
-        }
-      },
-      images: {
-        files: ['images/**']
-      },
-      css: {
-        files: ['css/{,**/}*.css']
-      },
-      js: {
-        files: ['js/{,**/}*.js', '!js/{,**/}*.min.js'],
-        tasks: ['jshint', 'uglify:dev']
-      }
-    },
+    global_vars: global_vars,
+    pkg: grunt.file.readJSON('package.json'),
 
-    shell: {
-      all: {
-        command: 'drush cache-clear theme-registry'
-      }
-    },
-
-    compass: {
-      options: {
-        config: 'config.rb',
-        bundleExec: true,
-        force: true
-      },
-      dev: {
-        options: {
-          environment: 'development'
-        }
-      },
+    sass: {
       dist: {
         options: {
-          environment: 'production'
+          outputStyle: 'compressed',
+          includePaths: ['<%= global_vars.theme_scss %>', '<%= global_vars.base_theme_path %>/scss/'].concat(bourbon)
+        },
+        files: {
+          '<%= global_vars.theme_css %>/<%= global_vars.theme_name %>.css': '<%= global_vars.theme_scss %>/<%= global_vars.theme_name %>.scss'
         }
       }
     },
@@ -61,62 +65,47 @@ module.exports = function (grunt) {
       options: {
         jshintrc: '.jshintrc'
       },
-      all: ['js/{,**/}*.js', '!js/{,**/}*.min.js']
+      all: [
+        'Gruntfile.js',
+        jsApp
+      ]
     },
 
     uglify: {
-      dev: {
-        options: {
-          mangle: false,
-          compress: false,
-          beautify: true
-        },
-        files: [{
-          expand: true,
-          flatten: true,
-          cwd: 'js',
-          dest: 'js',
-          src: ['**/*.js', '!**/*.min.js'],
-          rename: function(dest, src) {
-            var folder = src.substring(0, src.lastIndexOf('/'));
-            var filename = src.substring(src.lastIndexOf('/'), src.length);
-            filename = filename.substring(0, filename.lastIndexOf('.'));
-            return dest + '/' + folder + filename + '.min.js';
-          }
-        }]
+      options: {
+        sourceMap: false
       },
       dist: {
+        files: {
+          'js/libs.min.js': [jsLibs],
+          'js/foundation.min.js': [jsFoundation],
+          'js/app.min.js': [jsApp]
+      }
+      }
+    },
+
+    watch: {
+      grunt: { files: ['Gruntfile.js'] },
+
+      sass: {
+        files: '<%= global_vars.theme_scss %>/**/*.scss',
+        tasks: ['sass'],
         options: {
-          mangle: true,
-          compress: true
-        },
-        files: [{
-          expand: true,
-          flatten: true,
-          cwd: 'js',
-          dest: 'js',
-          src: ['**/*.js', '!**/*.min.js'],
-          rename: function(dest, src) {
-            var folder = src.substring(0, src.lastIndexOf('/'));
-            var filename = src.substring(src.lastIndexOf('/'), src.length);
-            filename = filename.substring(0, filename.lastIndexOf('.'));
-            return dest + '/' + folder + filename + '.min.js';
-          }
-        }]
+          livereload: true
+        }
+      },
+
+      js: {
+        files: [
+          jsLibs,
+          jsFoundation,
+          '<%= jshint.all %>'
+        ],
+        tasks: ['jshint', 'uglify']
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-shell');
-
-  grunt.registerTask('build', [
-    'uglify:dist',
-    'compass:dist',
-    'jshint'
-  ]);
-
+  grunt.registerTask('build', ['jshint','uglify','sass']);
+  grunt.registerTask('default', ['build', 'watch']);
 };
--- sites/all/themes/uylong/template.php
+++ sites/all/themes/uylong/template.php
@@ -1,30 +1,223 @@
 <?php
 
 /**
- * @file
- * Template overrides as well as (pre-)process and alter hooks for the Ohm
- * theme.
+ * Implements template_preprocess_html().
  */
+function uylong_links__topbar_main_menu($variables) {
+  // We need to fetch the links ourselves because we need the entire tree.
+  $links = menu_tree_output(menu_tree_all_data(variable_get('menu_main_links_source', 'main-menu')));
+  $output = _zurb_foundation_links($links);
+  $variables['attributes']['class'][] = 'left';
+
+  return '<ul' . drupal_attributes($variables['attributes']) . '>' . $output . '</ul>';
+}
 
 /**
- * Check if a block is a menu block or not.
- *
- * @param stdClass $block
- *   A block object.
- *
- * @return bool
- *   Given block is a menu block.
+ * Implements template_preprocess_page.
  */
-function _ohm_is_menu_block($block) {
-  $modules = array('menu', 'menu_block');
-  if (in_array($block->module, $modules)) {
-    return TRUE;
+function uylong_preprocess_page(&$variables) {
+    if (!empty($variables['node'])) {
+    $variables['theme_hook_suggestions'][] = 'page__' . $variables['node']->type;
   }
 
-  $modules = array('help', 'powered-by', 'main');
-  if ($block->module == 'system' && !in_array($block->delta, $modules)) {
-    return TRUE;
+  $variables['logo_img'] = '';
+  if (!empty($variables['logo'])) {
+    $variables['logo_img'] = theme('image', array(
+      'path' => $variables['logo'],
+      'alt' => strip_tags($variables['site_name']) . ' ' . t('logo'),
+      'title' => strip_tags($variables['site_name']) . ' ' . t('Home'),
+      'attributes' => array(
+        'class' => array('logo'),
+      ),
+    ));
   }
 
-  return FALSE;
+  $variables['linked_logo'] = '';
+  if (!empty($variables['logo_img'])) {
+    $variables['linked_logo'] = l($variables['logo_img'], '<front>', array(
+      'attributes' => array(
+        'rel' => 'home',
+        'title' => strip_tags($variables['site_name']) . ' ' . t('Home'),
+      ),
+      'html' => TRUE,
+    ));
+  }
+
+  $variables['linked_site_name'] = '';
+  if (!empty($variables['site_name'])) {
+    $variables['linked_site_name'] = l($variables['site_name'], '<front>', array(
+      'attributes' => array(
+        'rel' => 'home',
+        'title' => strip_tags($variables['site_name']) . ' ' . t('Home'),
+      ),
+      'html' => TRUE,
+    ));
+  }
+
+  // Top bar.
+  if ($variables['top_bar'] = theme_get_setting('zurb_foundation_top_bar_enable')) {
+    $top_bar_classes = array();
+
+    if (theme_get_setting('zurb_foundation_top_bar_grid')) {
+      $top_bar_classes[] = 'contain-to-grid';
+    }
+
+    if (theme_get_setting('zurb_foundation_top_bar_sticky')) {
+      $top_bar_classes[] = 'sticky';
+    }
+
+    if ($variables['top_bar'] == 2) {
+      $top_bar_classes[] = 'show-for-small';
+    }
+
+    $variables['top_bar_classes'] = implode(' ', $top_bar_classes);
+    $variables['top_bar_menu_text'] = check_plain(theme_get_setting('zurb_foundation_top_bar_menu_text'));
+
+    $top_bar_options = array();
+
+    if (!theme_get_setting('zurb_foundation_top_bar_custom_back_text')) {
+      $top_bar_options[] = 'custom_back_text:false';
+    }
+
+    if ($back_text = check_plain(theme_get_setting('zurb_foundation_top_bar_back_text'))) {
+      if ($back_text !== 'Back') {
+        $top_bar_options[] = "back_text:'{$back_text}'";
+      }
+    }
+
+    if (!theme_get_setting('zurb_foundation_top_bar_is_hover')) {
+      $top_bar_options[] = 'is_hover:false';
+    }
+
+    if (!theme_get_setting('zurb_foundation_top_bar_scrolltop')) {
+      $top_bar_options[] = 'scrolltop:false';
+    }
+
+    if (theme_get_setting('zurb_foundation_top_bar_mobile_show_parent_link')) {
+      $top_bar_options[] = 'mobile_show_parent_link:true';
+    }
+
+    $variables['top_bar_options'] = ' data-options="' . implode('; ', $top_bar_options) . '"';
+  }
+
+  // Alternative header.
+  // This is what will show up if the top bar is disabled or enabled only for
+  // mobile.
+  if ($variables['alt_header'] = ($variables['top_bar'] != 1)) {
+    // Hide alt header on mobile if using top bar in mobile.
+    $variables['alt_header_classes'] = $variables['top_bar'] == 2 ? ' hide-for-small' : '';
+  }
+
+  // Menus for alternative header.
+  $variables['alt_main_menu'] = '';
+
+  if (!empty($variables['main_menu'])) {
+    $variables['alt_main_menu'] = theme('links__system_main_menu', array(
+      'links' => $variables['main_menu'],
+      'attributes' => array(
+        'id' => 'main-menu-links',
+        'class' => array('links', 'inline-list', 'clearfix'),
+      ),
+      'heading' => array(
+        'text' => t('Main menu'),
+        'level' => 'h2',
+        'class' => array('element-invisible'),
+      ),
+    ));
+  }
+
+  $variables['alt_secondary_menu'] = '';
+
+  if (!empty($variables['secondary_menu'])) {
+    $variables['alt_secondary_menu'] = theme('links__system_secondary_menu', array(
+      'links' => $variables['secondary_menu'],
+      'attributes' => array(
+        'id' => 'secondary-menu-links',
+        'class' => array('links', 'clearfix'),
+      ),
+      'heading' => array(
+        'text' => t('Secondary menu'),
+        'level' => 'h2',
+        'class' => array('element-invisible'),
+      ),
+    ));
+  }
+
+  // Top bar menus.
+  //$variables['top_bar_main_menu'] = '';
+  
+    $variables['top_bar_main_menu'] = theme('links__topbar_main_menu', array(
+      'links' => $variables['main_menu'],
+      'attributes' => array(
+        'id' => 'main-menu',
+        'class' => array('main-nav'),
+      ),
+      'heading' => array(
+        'text' => t('Main menu'),
+        'level' => 'h2',
+        'class' => array('element-invisible'),
+      ),
+    ));
+ 
+
+  $variables['top_bar_secondary_menu'] = '';
+  if (!empty($variables['secondary_menu'])) {
+    $variables['top_bar_secondary_menu'] = theme('links__topbar_secondary_menu', array(
+      'links' => $variables['secondary_menu'],
+      'attributes' => array(
+        'id' => 'secondary-menu',
+        'class' => array('secondary', 'link-list'),
+      ),
+      'heading' => array(
+        'text' => t('Secondary menu'),
+        'level' => 'h2',
+        'class' => array('element-invisible'),
+      ),
+    ));
+  }
+
+  // Messages in modal.
+  $variables['zurb_foundation_messages_modal'] = theme_get_setting('zurb_foundation_messages_modal');
+
+  // Convenience variables.
+  if (!empty($variables['page']['sidebar_first'])) {
+    $left = $variables['page']['sidebar_first'];
+  }
+
+  if (!empty($variables['page']['sidebar_second'])) {
+    $right = $variables['page']['sidebar_second'];
+  }
+
+  // Dynamic sidebars.
+  if (!empty($left) && !empty($right)) {
+    $variables['main_grid'] = 'medium-6 medium-push-3';
+    $variables['sidebar_first_grid'] = 'medium-3 medium-pull-6';
+    $variables['sidebar_sec_grid'] = 'medium-3';
+  }
+  elseif (empty($left) && !empty($right)) {
+    $variables['main_grid'] = 'medium-9';
+    $variables['sidebar_first_grid'] = '';
+    $variables['sidebar_sec_grid'] = 'medium-3';
+  }
+  elseif (!empty($left) && empty($right)) {
+    $variables['main_grid'] = 'medium-9 medium-push-3';
+    $variables['sidebar_first_grid'] = 'medium-3 medium-pull-9';
+    $variables['sidebar_sec_grid'] = '';
+  }
+  else {
+    $variables['main_grid'] = '';
+    $variables['sidebar_first_grid'] = '';
+    $variables['sidebar_sec_grid'] = '';
+  }
+
+  // Ensure modal reveal behavior if modal messages are enabled.
+  if (theme_get_setting('zurb_foundation_messages_modal')) {
+    drupal_add_js(drupal_get_path('theme', 'zurb_foundation') . '/js/behavior/reveal.js');
+  }
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function uylong_preprocess_node(&$variables) {
 }
